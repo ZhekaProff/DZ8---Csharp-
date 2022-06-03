2621f24027a52d5f29237d30d6eccfc76e1f52aa
ZhekaProff <Zhekakad@ya.ru>
2022-06-03 14:32:49 +0900

    решение первой задачи

---
+++ Task01/Program.cs
@@ -0,0 +1,51 @@
+﻿// Найти произведение двух матриц
+
+void FillMatrix( int[,] matrix) // Заполняем рандомными числами
+{
+    for ( int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for ( int j = 0; j < matrix.GetLength(1) ; j++)
+        {
+            matrix[i, j] = new Random().Next(0,9); 
+        }
+    
+    }
+
+
+}
+
+void PrintMatrix(int [,] matrix) // функция печати матрицы
+{
+    for ( int  i = 0; i < matrix.GetLength(0); i++)
+    {
+        for ( int j = 0; j < matrix.GetLength(1) ; j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+int Summa( int [,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+    }
+    return sum;
+}
+Console.Clear();
+int [,] matrix  = new int[2, 3];
+int [,] matrix2 = new int[4, 2];
+
+FillMatrix(matrix);
+FillMatrix(matrix2);
+PrintMatrix(matrix);
+Console.WriteLine();
+PrintMatrix(matrix2);
+Console.WriteLine($"Произведение двух матриц ровно: {Summa(matrix) * Summa(matrix2)} ");
+
