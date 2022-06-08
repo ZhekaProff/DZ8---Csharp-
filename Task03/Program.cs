a1ee5a138616919a045e189ef019702a777ca960
ZhekaProff <Zhekakad@ya.ru>
2022-06-08 13:41:28 +0900

    решение третьей задачи

---
+++ Task03/Program.cs
@@ -0,0 +1,39 @@
+﻿// Сформировать трехмерный массив не повторяющимися двузначными числами 
+// показать его построчно на экран выводя индексы соответствующего элемента
+
+Console.Clear();
+
+void FillMatrix( int[, ,] matrix) // Заполняем рандомными числами
+{
+    for ( int  i = 0; i < matrix.GetLength(0); i++)
+    {
+        for ( int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for ( int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = new Random().Next(10,20); 
+
+            }
+        }
+    }    
+}
+
+
+void PrintMatrix(int [,,] matrix) // функция печати матрицы
+{
+    for ( int  i = 0; i < matrix.GetLength(0); i++)
+    {
+        for ( int j = 0; j < matrix.GetLength(1) ; j++)
+        {
+            for ( int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"Элемент[{i},{j},{k}] = {matrix[i, j, k]} ");
+            }
+        Console.WriteLine();
+        }
+    }
+}
+
+int[, ,] matrix = new int[2, 2, 2];
+FillMatrix(matrix);
+PrintMatrix(matrix);
